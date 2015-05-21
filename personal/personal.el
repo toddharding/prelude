364a56834651ef665ce1a1fcd4af025b6ebb99a6
toddharding <t.harding@dundee.ac.uk>
2015-05-21 06:44:08 -0700

    Added start emacs server on open

--- personal/personal.el
+++ personal/personal.el
@@ -6,3 +6,6 @@
 (electric-indent-mode 1)
 (prelude-require-package 'solarized-theme)
 (setq prelude-theme 'solarized-dark)
+(if (and (fboundp 'server-running-p)
+         (not (server-running-p)))
+    (server-start))
